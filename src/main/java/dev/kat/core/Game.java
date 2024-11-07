1022230cf6d4c38fc22e61515c1124c8dbaeb12f
KatarinaMantay <katarinamantay@gmail.com>
2024-11-07 15:46:05 -0500

    UPS addition

--- src/main/java/dev/kat/core/Game.java
+++ src/main/java/dev/kat/core/Game.java
@@ -5,6 +5,7 @@ public class Game implements Runnable {
     private Panel panel;
     private Thread thread;
     private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
 
     public Game(){
         panel = new Panel();
@@ -23,13 +24,29 @@ public class Game implements Runnable {
     public void run() {
 
         double timePerFrame = 1000000000.0/FPS_SET;
+        double timePerUpdate = 1000000000.0/UPS_SET;
+
+        long previousTime = System.nanoTime();
+
         long lastFrame = System.nanoTime();
         long now = System.nanoTime();
+        int updates = 0;
         int frames = 0;
         long lastCheck = System.currentTimeMillis();
 
+        double deltaU = 0;
+
         while(true){
             now = System.nanoTime();
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            previousTime = currentTime;
+
+            if(deltaU >= 1){
+                updates++;
+                deltaU--;
+            }
 
             if(now - lastFrame  >= timePerFrame){
                 panel.repaint();
@@ -39,8 +56,9 @@ public class Game implements Runnable {
 
             if(System.currentTimeMillis() - lastCheck >= 1000){
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
 
         }
